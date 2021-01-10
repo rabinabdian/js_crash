da9fca235aa9635a20ccb3210dd933b34738f539
Ruben <rabin.abdian@gmail.com>
2021-01-10 16:07:27 +0200

    constructor function

--- src/index.js
+++ src/index.js
@@ -17,3 +17,14 @@ function createCircle(radius) {
 const circle = createCircle(1);
 
 circle.draw();
+
+// constructor function
+
+function Circle(radius) {
+  this.radius = radius;
+  this.draw = function () {
+    console.log("draw from const func");
+  };
+}
+const c1 = new Circle(3);
+c1.draw();
