67d2a90439ecf6012e45a6b812419a06ae39f822
rabinabdian <rabin.abdian@gmail.com>
2021-01-11 00:32:42 +0200

    dom

---
+++ src/02 dom properties and methods2 finish/app.js
@@ -0,0 +1,21 @@
+let doc
+doc = document
+doc = document.all[1]
+doc = document.head
+doc = document.body
+doc = document.doctype
+doc = document.domain
+doc = document.URL
+doc = document.characterSet
+doc = document.contentType
+doc = document.links
+doc = document.links[0].classList
+doc = document.links[0].className
+doc = document.images
+doc = document.scripts
+let scripts = document.scripts
+let scriptsArray = Array.from(scripts)
+scriptsArray.forEach(function (script, index) {
+    console.log(index + script.getAttribute('src'))
+})
+console.log(doc)
