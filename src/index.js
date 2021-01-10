9aea60e1a29edeafae428cdb2d50bbe22d1754a2
rabinabdian <rabin.abdian@gmail.com>
2021-01-10 16:52:05 +0200

    print probs  keys

--- src/index.js
+++ src/index.js
@@ -24,3 +24,34 @@ c1.draw();
 
 Circle.call({}, 1);
 Circle.apply({}, [1, 2, 3]);
+
+// print properties of class 
+console.log('------------------ print properties & funcs of class ');
+for (let key in circle) {
+    console.log(key, circle[key]);
+
+}
+
+// print properties of class 
+console.log('------------------ print only properties of class ');
+for (let key in circle) {
+
+    if (typeof circle[key] !== 'function') {
+        console.log(key, circle[key]);
+
+    }
+
+}
+
+// print properties & funcs of class by object keys
+console.log('------------------ print properties & funcs of class by object keys ');
+const keys = Object.keys(circle);
+console.log(keys);
+
+
+// print properties only
+console.log('------------------ print properties only ');
+if ('radius' in circle) {
+    console.log('circle has radius ');
+
+}
