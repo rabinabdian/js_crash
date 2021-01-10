4e6aed9b75c7a5fd65076118cb30f8190004555d
rabinabdian <rabin.abdian@gmail.com>
2021-01-10 16:20:34 +0200

    support es6

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
-import "./styles.css";
-
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-
-`;
-
 // factory function
 function createCircle(radius) {
-  return {
-    radius,
-    draw: function () {
-      console.log("draw circle with radius : " + radius);
-    }
-  };
+    return {
+        radius,
+        draw: function() {
+            console.log("draw circle with radius : " + radius);
+        }
+    };
 }
 const circle = createCircle(1);
 
@@ -21,10 +14,10 @@ circle.draw();
 // constructor function
 
 function Circle(radius) {
-  this.radius = radius;
-  this.draw = function () {
-    console.log("draw from const func");
-  };
+    this.radius = radius;
+    this.draw = function() {
+        console.log("draw from const func");
+    };
 }
 const c1 = new Circle(3);
 c1.draw();
