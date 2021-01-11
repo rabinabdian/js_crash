1343d7feb365d83fb957cc7ed2204ae3ca4551e8
rabinabdian <rabin.abdian@gmail.com>
2021-01-11 19:54:04 +0200

    dom

--- src/01 dom properties and methods finish/app.js
+++ src/01 dom properties and methods finish/app.js
@@ -1,11 +1,11 @@
 let doc
-doc = document
-doc = document.all[1]
-doc = document.head
-doc = document.body
-doc = document.doctype
-doc = document.domain
-doc = document.URL
+    //doc = document
+    //doc = document.all[1]
+    //doc = document.head
+    // doc = document.body
+    //doc = document.doctype
+    //doc = document.domain   // 127.0.0.1
+    //doc = document.URL // http://127.0.0.1:5500/js_crash/src/01%20dom%20properties%20and%20methods%20finish/index.html
 doc = document.characterSet
-doc = document.contentType
+    // doc = document.contentType
 console.log(doc)
