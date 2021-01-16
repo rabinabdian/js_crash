b9ab92d25b5f0cc641645b22728b5a796103d414
rabinabdian <rabin.abdian@gmail.com>
2021-01-16 14:38:45 +0200

    xhr different readystate

--- src/01+xhr+object/01 xhr object/app.js
+++ src/01+xhr+object/01 xhr object/app.js
@@ -1,16 +1,13 @@
 document.getElementById('button').addEventListener('click', loadData);
-var div = document.getElementById('output');
 
 function loadData() {
     const xhr = new XMLHttpRequest();
 
     xhr.open('GET', 'data.txt', true);
 
-    xhr.onload = function() {
+    xhr.onload = function () {
         if (this.status === 200) {
             console.log(this.responseText)
-            div.innerHTML += this.responseText;
-
         }
     }
 
