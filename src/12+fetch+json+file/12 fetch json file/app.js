6d0469e8d404c63df9a6a96ec06bff96eb575763
rabinabdian <rabin.abdian@gmail.com>
2021-01-18 13:14:50 +0200

    fetch api to fetch json

---
+++ src/12+fetch+json+file/12 fetch json file/app.js
@@ -0,0 +1,28 @@
+document.getElementById('button1').addEventListener('click', getText)
+document.getElementById('button2').addEventListener('click', getJSON)
+
+function getText() {
+
+    fetch('test.txt').then(function(res) {
+        return (res.text())
+    }).then(function(data) {
+        document.getElementById('output').innerText = data
+    }).catch(function(err) {
+        console.log('ERROR: ' + err)
+    })
+}
+
+function getJSON() {
+
+    fetch('todos.json').then(function(res) {
+        return (res.json())
+    }).then(function(todos) {
+        let output = '';
+        todos.forEach(todo => {
+            output += `<label>Title : ${todo.title} </br>Body :  ${todo.body}</label></br>`
+        });
+        document.getElementById('output').innerHTML = output
+    }).catch(function(err) {
+        console.log('ERROR: ' + err)
+    })
+}
