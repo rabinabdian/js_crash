bcf0fd512c4e0ebf4e3205776bfbb721f9d6098b
rabinabdian <rabin.abdian@gmail.com>
2021-01-19 11:07:44 +0200

    arrow function

--- src/12+fetch+json+file/12 fetch json file/app.js
+++ src/12+fetch+json+file/12 fetch json file/app.js
@@ -1,28 +1,27 @@
 document.getElementById('button1').addEventListener('click', getText)
 document.getElementById('button2').addEventListener('click', getJSON)
-
 function getText() {
 
-    fetch('test.txt').then(function(res) {
+    fetch('test.txt').then(function (res) {
         return (res.text())
-    }).then(function(data) {
+    }).then(function (data) {
         document.getElementById('output').innerText = data
-    }).catch(function(err) {
+    }).catch(function (err) {
         console.log('ERROR: ' + err)
     })
 }
 
 function getJSON() {
 
-    fetch('todos.json').then(function(res) {
+    fetch('todos.json').then(function (res) {
         return (res.json())
-    }).then(function(todos) {
+    }).then(function (todos) {
         let output = '';
         todos.forEach(todo => {
-            output += `<label>Title : ${todo.title} </br>Body :  ${todo.body}</label></br>`
+            output += `<li>${todo.title}</li>`
         });
         document.getElementById('output').innerHTML = output
-    }).catch(function(err) {
+    }).catch(function (err) {
         console.log('ERROR: ' + err)
     })
 }
