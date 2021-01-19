3f30403cc2d0e284b5235088f32a5ed121f5332e
rabinabdian <rabin.abdian@gmail.com>
2021-01-19 23:38:05 +0200

    Async await

--- src/16+custom+http+with+fetch/16 custom http with fetch/app.js
+++ src/16+custom+http+with+fetch/16 custom http with fetch/app.js
@@ -5,8 +5,7 @@ const data = {
     title: ' A new title',
     body: 'This is a new post'
 }
- //http.post('https://jsonplaceholder.typicode.com/posts', data).then(data => console.log(data)).catch(err => console.log(err))
-//http.put('https://jsonplaceholder.typicode.com/posts/1', data).then(data => console.log(data)).catch(err => console.log(err))
-http.get('https://jsonplaceholder.typicode.com/posts/1').then(data => console.log(data)).catch(err => console.log(err))
-//http.delete('https://jsonplaceholder.typicode.com/posts/1').then(data => console.log(data)).catch(err => console.log(err))
+// http.post('https://jsonplaceholder.typicode.com/posts', data).then(data => console.log(data)).catch(err => console.log(err))
+// http.put('https://jsonplaceholder.typicode.com/posts/1', data).then(data => console.log(data)).catch(err => console.log(err))
+http.delete('https://jsonplaceholder.typicode.com/posts/1').then(data => console.log(data)).catch(err => console.log(err))
 
--- src/script.js
+++ src/script.js
@@ -1,13 +1,34 @@
-document.getElementById('btn1').addEventListener('click', getText)
+// document.getElementById('btn1').addEventListener('click', getText)
 
 
 
-const getText = () => {
-    fetch(text.txt).then(function(res) {
-        return (res.text())
-    }).then(function(data) {
-        document.getElementById('output').innerText = data
-    }).catch(function(err) {
-        console.log('ERROR: ' + err)
-    })
+// const getText = () => {
+//     fetch(text.txt).then(function(res) {
+//         return (res.text())
+//     }).then(function(data) {
+//         document.getElementById('output').innerText = data
+//     }).catch(function(err) {
+//         console.log('ERROR: ' + err)
+//     })
+// }
+
+
+// async function message() {
+//     return 'you have new message'
+// }
+
+// console.log(message())
+
+
+// const message = async  () =>  'you have new message'
+
+// message().then(res => console.log(res))
+
+const getTodos = async() => {
+    const res = await  fetch('https://jsonplaceholder.typicode.com/todos')
+
+    const data = await res.json()
+    return data
 }
+
+getTodos().then(todos => console.log(todos))
