8de4e129964a3093ee1660a9721ed80a265595f1
rabinabdian <rabin.abdian@gmail.com>
2021-01-20 20:28:41 +0200

    err

--- src/03+regular+expression+1/03 regular expression 1/app.js
+++ src/03+regular+expression+1/03 regular expression 1/app.js
@@ -4,33 +4,26 @@ console.log(re.source)
 
 //exec()
 // const result = re.exec('Hello everyone')
-
-// console.log(result) // [ 'Hello', index: 0, input: 'Hello everyone', groups: undefined ]
-// console.log('result[0]')
-
-// console.log(result[0]) // Hello
-// console.log('result.index')
-
-// console.log(result.index) // 0 
-// console.log('result.input')
-
-// console.log(result.input) // Hello everyone
+// // console.log(result)
+// // console.log(result[0])
+// // console.log(result.index)
+// console.log(result.input)
 
 //match()
 // const string = 'Hello, everyone, Hello';
 // const result = string.match(re)
-// console.log(result)  // [ 'Hello', 'Hello' ]
+// console.log(result)
 
 //test()
 // const result = re.test('Hello')
-// console.log(result) // true 
+// console.log(result)
 
 //search()
 // const string = 'Hi, Hello, Hello';
 // const result = string.search(re)
-// console.log(result)  //  4
+// console.log(result)
 
 //replace()
 const string = 'Hi, Hello, Hello';
 const result = string.replace(re, 'hey');
-console.log(result)  // Hi, hey, hey
+console.log(result)
